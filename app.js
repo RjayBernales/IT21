1847808ddb7fc5da795d9a4831421c86700792fd
RJayBernales <rainieljevson0@gmail.com>
2024-10-04 23:14:20 +0800

    Added a Process and output solution using console.log

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+
+//Inputs
+console.log("I'm RJ, a student at NBSC");
+
+const person1={
+    Name: "RJ",
+    age: 18,
+    hobby: "Reading"
+}
+
+//Process and Output
+console.log("Hello!!")
+console.log("Name: " + person1.name)
+console.log("Hello!!" + person1.age)
+console.log("Hello!!" + person.hobby)
