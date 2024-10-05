ad4f6c7ebad1b3aba8f78d2f0ab94ac7897f5146
RJayBernales <rainieljevson0@gmail.com>
2024-10-05 23:40:17 +0800

    Added class and methods

--- Methods.js
+++ Methods.js
@@ -10,3 +10,28 @@ const art = {
    console.log("Type: "  +art.type);
    console.log("Entry No.: " + art.entry);  
    console.log("~~~~~~~~~~~~~~~~~~~~~~~~");
+
+   //Adding class
+   class Person {
+    constructor(name, type, entry){
+        this.name = name;
+        this.age = type;
+        this.hobby = entry;
+    }
+
+    //Solution 1 displaying using console.log
+    displayInfo(){
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.type);
+        console.log("Hobby: " + this.entry);
+     }
+    //Solution 2 Displaying via html
+    displayViaHTML() {
+        const personElement = document.getElementById('person'); 
+        personElement.innerHTML = `
+        <strong>Name:</strong> ${this.name}<br>
+        <strong>Age:</strong> ${this.type}<br>
+        <strong>Hobby:</strong> ${this.entry}
+        `   ;
+    }
+}
