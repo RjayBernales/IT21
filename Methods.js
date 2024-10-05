26dd5d64cf56c3d1f3f9d98cb6817de72fc39c5f
RJayBernales <rainieljevson0@gmail.com>
2024-10-05 23:33:23 +0800

    Added object and display it via console.log

---
+++ Methods.js
@@ -0,0 +1,12 @@
+const art = {
+    name: "Rain",
+    type: "Landscape",
+    entry: 1
+   }
+
+   //Process and Output
+   console.log("Art Registration Details");
+   console.log("Name: " + art.name);
+   console.log("Type: "  +art.type);
+   console.log("Entry No.: " + art.entry);  
+   console.log("~~~~~~~~~~~~~~~~~~~~~~~~");
