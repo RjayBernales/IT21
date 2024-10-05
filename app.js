e211996a1c8b37451cabea55631bbe360f4f560c
RJayBernales <rainieljevson0@gmail.com>
2024-10-05 14:56:44 +0800

    Added case 4 which acts as a button for user to exit and default incase the user's input is invalid

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Inputs
-const person1={
+const person ={
     Name: "RJ",
-    age: 18,
+    age: 19,
     hobby: "Reading"
 }
 
@@ -10,3 +10,32 @@ console.log("Hello!!")
 console.log("Name: " + person1.Name)
 console.log("Age: " + person1.age)
 console.log("Hobby: " + person1.hobby)
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+//Adding class named Person
+class Person{
+        constructor (Name, age, hobby) {
+            this.Name= Name;
+            this.age = age;
+            this.hobby= hobby;
+        }
+        //Solution 1/Console.log
+        DisplayInfo(){
+            console.log("Name: " + person1.Name);
+            console.log("Age" + person1.age);
+            console.log("Hobby" + person1.hobby);
+        }
+        //Solution 2/HTML
+        DisplayViaHTML(){
+            const personElement = document.getElementById('person1');
+            personElement.innerHTML =`
+            <strong>Name:</strong> ${this.Name}<br> 
+            <strong>Age:</strong> ${this.age}<br> 
+            <strong>Name:</strong> ${this.hobby}<br> 
+            `;
+        }
+    }
+    //creating instance for person
+    const person1 = new Person(RJ, 19, Reading)
+    //Calling Method
+    person1.DisplayInfo();
