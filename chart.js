bbf0c6c31a2d3ec4fca03da4d81f8b677199a500
Rjay Bernales <rainieljevson0@gmail.com>
2024-10-15 18:13:16 +0800

    added new class for the bar chart's function

---
+++ chart.js
@@ -0,0 +1,30 @@
+class ChartCreator {
+    constructor(dataUrl) {
+        this.dataUrl = dataUrl;
+        this.chartData = null;
+    }
+
+    async init() {
+        await this.fetchData();
+        if (this.chartData) {
+            this.createCharts();
+        }
+    }
+
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            this.chartData = await response.json();
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    }
+
+    createCharts() {
+        // This method will be overridden in subclasses
+        throw new Error('createCharts() must be implemented in subclasses');
+    }
+}
