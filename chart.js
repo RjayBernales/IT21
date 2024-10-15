99541d720155c7d85f9a51c29931236caccb76c8
Rjay Bernales <rainieljevson0@gmail.com>
2024-10-15 18:15:31 +0800

    added subclass that connects to the parent class

--- chart.js
+++ chart.js
@@ -28,3 +28,67 @@ class ChartCreator {
         throw new Error('createCharts() must be implemented in subclasses');
     }
 }
+class BarChart extends ChartCreator {
+    constructor(dataUrl) {
+        super(dataUrl);
+        this.barCtx = document.getElementById('barChart');
+    }
+
+    createCharts() {
+        this.createBarChart();
+    }
+
+    createBarChart() {
+        const chartData = {
+            labels: this.chartData.labels,
+            datasets: [{
+                label: 'Number of Students',
+                data: this.chartData.data,
+                backgroundColor: 'rgba(54, 162, 235, 0.5)',  
+                borderColor: 'rgba(54, 162, 235, 1)',  
+                borderWidth: 1
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data: chartData,
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        grid: {
+                            color: '#e0e0e0'
+                        },
+                        ticks: {
+                            color: '#495057'
+                        }
+                    },
+                    x: {
+                        grid: {
+                            color: '#e0e0e0'
+                        },
+                        ticks: {
+                            color: '#495057'
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'top',
+                        labels: {
+                            color: '#333'
+                        }
+                    },
+                    tooltip: {
+                        enabled: true
+                    }
+                }
+            }
+        };
+
+        new Chart(this.barCtx, config); 
+    }
+}
